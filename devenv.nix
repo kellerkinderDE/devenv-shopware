dbe17638cca930381f7073edecfa5684266bf8d2
Alex <git@jinnoflife.com>
2023-08-24 10:18:32 +0200

    Increase node version from 16 to 18

--- devenv.nix
+++ devenv.nix
@@ -274,7 +274,7 @@ in {
 
     languages.javascript = {
       enable = lib.mkDefault true;
-      package = lib.mkDefault pkgs.nodejs-16_x;
+      package = lib.mkDefault pkgs.nodejs-18_x;
     };
 
     languages.php = {
